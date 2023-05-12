2fa81dcefb8b218981cd13381dc5882351b94c97
jessika <1355943+jessika@users.noreply.github.com>
2023-05-11 22:39:24 -0700

    Fixed lint errors.
    - No unescaped single quotes in React template
    - hello.ts has lint errors and was not used

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 
+const aboutBlurb = `I'm Jess, and this is my personal blog. I'll be writing some travel reports among other things.`
+
 export default function About() {
   return <Layout>
       <Head>
         <title>About</title>
       </Head>
-      I'm Jess, and this is my personal blog. I'll be writing some travel reports among other things.
+      {aboutBlurb}
     </Layout>
 }
