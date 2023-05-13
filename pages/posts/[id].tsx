16b626e8823405aaaa9af947fda7eac05a6ecaeb
jessika <1355943+jessika@users.noreply.github.com>
2023-05-12 23:11:09 -0700

    Convert md to mdx. No uservisible changes.
    - Locally confirmed that React components can be added to mdx posts.
    - Used this guide https://nextjs.org/blog/markdown

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,21 +1,12 @@
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import { getAllPostIds, getPostData, PostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
-import Link from "next/link";
+import { MDXRemote } from "next-mdx-remote";
 
-export default function Post({
-  postData,
-}: {
-  postData: {
-    contentHtml: string;
-    title: string;
-    updated_date: string;
-    visited_date: string;
-  };
-}) {
+export default function Post({ postData }: { postData: PostData }) {
   return (
     <Layout showBottomHomeLink>
       <Head>
@@ -27,14 +18,17 @@ export default function Post({
           Visited{" "}
           <Date dateString={postData.visited_date} formatString={"LLLL yyyy"} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <main>
+          <MDXRemote {...postData.mdxSource} />
+        </main>
       </article>
     </Layout>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllPostIds();
+  const ids = getAllPostIds();
+  const paths = ids.map((id) => ({ params: { id } }));
   return {
     paths,
     fallback: false,
