37234617a19cdfa44cd259504baa821e7adc9ccc
jessika <1355943+jessika@users.noreply.github.com>
2023-05-15 16:16:22 -0700

    Add contact info

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 
-const aboutBlurb = `I'm Jess, and this is my personal blog. I'll be writing some travel reports among other things.`;
+const aboutBlurb = `Hello there! I've created this little corner in the interwebs to document my travels and outdoor explorations. Feel free to leave me a comment at jessgoesoutside.blog@gmail.com. -Jess`;
 
 export default function About() {
   return (
