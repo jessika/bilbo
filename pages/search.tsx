791d8b580dc57a1807919ed76db9f1848164fce9
jessika <1355943+jessika@users.noreply.github.com>
2023-05-31 10:23:12 -0700

    Reduce dependencies in search page `useEffect` hooks

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,53 +4,67 @@ import { useRouter } from "next/router";
 import Searchbox from "../components/searchbox";
 import { useEffect, useState } from "react";
 import PostList from "../components/post-list";
-import styles from "./search.module.scss";
 
 /** Url key whose value is the search text. */
 const searchTextKey = "q";
 
 export default function Search({}: {}) {
-  // TODO: Add loading icon
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const qValue = router.query[searchTextKey] || "";
-  const initialSearchText =
-    typeof qValue === "string"
-      ? (qValue as string)
-      : (qValue as string[]).join(",");
-  const [searchText, setSearchText] = useState(initialSearchText);
+  const [searchText, setSearchText] = useState("");
   const [results, setResults] = useState([]);
 
-  const handleSearchTextUpdate = (searchText: string) => {
-    if (!searchText) return;
-    if (!searchText.trim()) return;
+  const [isSearchTextInitialized, setIsSearchTextInitialized] = useState(false);
 
-    setIsLoading(true);
-    setSearchText(searchText);
-    setResults([]);
+  useEffect(
+    function onSearchTextChange() {
+      if (!searchText) return;
 
-    fetch(`/api/search?q=${searchText}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setResults(res.postMetadatas);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-    // TODO: Detect url query change when going back in browser history
-    router.replace(
-      {
-        pathname: router.pathname,
-        query: { [searchTextKey]: searchText },
-      },
-      /* as= */ undefined,
-      { shallow: true }
-    );
+      setIsLoading(true);
+      setResults([]);
+      fetch(`/api/search?q=${searchText}`)
+        .then((res) => res.json())
+        .then((res) => {
+          setResults(res.postMetadatas);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+      // TODO: Detect url query change when going back in browser history
+      router.replace(
+        {
+          pathname: router.pathname,
+          query: { [searchTextKey]: searchText },
+        },
+        /* as= */ undefined,
+        { shallow: true }
+      );
+    },
+    [searchText]
+  );
+
+  const onSearchboxTextChange = (searchboxText: string) => {
+    if (!searchboxText) return;
+    searchboxText = searchboxText.trim();
+    if (!searchboxText) return;
+    setSearchText(searchboxText);
   };
 
-  useEffect(() => {
-    handleSearchTextUpdate(initialSearchText);
-  }, [router.isReady]);
+  // ignore exhaustive-deps warning for `router` and `router.query`. We only want the function to run once the router is ready, not when the query changes.
+  useEffect(
+    function initializeSearchText() {
+      const qValue = router.query[searchTextKey] || "";
+      const initialSearchText =
+        typeof qValue === "string"
+          ? (qValue as string)
+          : (qValue as string[]).join(",");
+      if (initialSearchText) {
+        setSearchText(initialSearchText);
+      }
+      setIsSearchTextInitialized(true);
+    },
+    [router.isReady]
+  );
 
   return (
     <Layout showBottomHomeLink>
@@ -58,12 +72,14 @@ export default function Search({}: {}) {
         <title>{`Search - ${siteTitle}`}</title>
       </Head>
       <section>
-        <Searchbox
-          initialText={initialSearchText}
-          placeholder={"Search..."}
-          onChange={handleSearchTextUpdate}
-          isLoading={isLoading}
-        />
+        {isSearchTextInitialized && (
+          <Searchbox
+            initialText={searchText}
+            placeholder={"Search..."}
+            onChange={onSearchboxTextChange}
+            isLoading={isLoading}
+          />
+        )}
       </section>
       {!!searchText && (
         <h2 style={{ whiteSpace: "pre-wrap" }}>
