f3ed0568e7f5353e8161891fec6b9b7a0700f310
jessika <1355943+jessika@users.noreply.github.com>
2023-06-22 20:31:15 -0700

    Make header sticky; add copyright footer;
    Update some meta titles

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import { siteTitle } from "../components/Layout";
 
 const aboutBlurb = `Hello there! I've created this little corner in the interwebs to document my travels and outdoor explorations. Feel free to leave me a comment at jessgoesoutside.blog@gmail.com. -Jess`;
 
@@ -7,7 +8,7 @@ export default function About() {
   return (
     <Layout>
       <Head>
-        <title>About</title>
+        <title>{`About | ${siteTitle}`}</title>
       </Head>
       {aboutBlurb}
     </Layout>
